d8cec8c4edfed0708ef2bac37c3e1f61dd03ab7e
Narend23023 <k.narendranath9640@gmail.com>
2024-08-03 12:59:21 +0530

    Update netapp_retrieval.py

--- netapp_retrieval.py
+++ netapp_retrieval.py
@@ -192,7 +192,7 @@ def img_prompt_func(data_dict):
 
               Instructions:
               1. Do not mention the context, text, or document in your response.
-              2. Provide detailed answers based on the information.
+              2. Provide detailed answers based on the information. Your only source of information required to answer will be in the context provided.
               3. Use bullet points where necessary.
               4. If there is zero information available, state: "There is not enough information to provide a complete answer to your question."
 
