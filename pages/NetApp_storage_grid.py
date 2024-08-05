598b7bfaa39d8fe4c172c38e84926ea243010fce
Narend23023 <k.narendranath9640@gmail.com>
2024-08-05 11:14:44 +0530

    Update NetApp_storage_grid.py

--- pages/NetApp_storage_grid.py
+++ pages/NetApp_storage_grid.py
@@ -196,7 +196,7 @@ def img_prompt_func(data_dict):
               2. Provide detailed answers based on the information. Your only source of information required to answer will be in the context provided and do not use your previous training to answer these questions.
               3. Use bullet points where necessary.
               4. If there is no exact information available specific to the question, state: "There is not enough information to provide a complete answer to your question."
-              5. Your Answers has to be elaborated yet accurate.
+              5. Your Answers has to be elaborated yet accurate. Do not give any open ended answers.
 
 
 Context:
