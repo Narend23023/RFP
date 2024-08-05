a3a1baf3b5d75c0c3c485e04a17bdc236303a1e8
Narend23023 <k.narendranath9640@gmail.com>
2024-08-05 16:23:45 +0530

    Update NetApp_storage_grid.py

--- pages/NetApp_storage_grid.py
+++ pages/NetApp_storage_grid.py
@@ -193,7 +193,7 @@ def img_prompt_func(data_dict):
 
               Instructions:
               1. If there is no relevant context or exact information available; the output should be “No Answer”. 
-              2. If Partial Answer is available; The output should include “Partial Answer Available” followed by the answer.
+              2. If Partial Answer is available; The output should include “Partial Answer Available” followed by the answer and also mention which aspect of the question you were unable to answer.
               3. Answer length should be not exceed more than 75-80 words or 7-8 lines.
               4. Do not mention the context, text, or document in your response.
               5. Your only source of information required to answer will be in the context provided and do not use your previous training to answer these questions.
